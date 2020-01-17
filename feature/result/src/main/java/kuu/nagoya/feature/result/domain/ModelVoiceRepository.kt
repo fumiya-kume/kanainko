928cafc06b80220a890382fcbfc9dd78a306dbff
Fumiya Kume <fumiya.kume@hotmail.com>
2020-01-17 12:56:44 +0900

    remove extra classes (#97)

--- feature/result/src/main/java/kuu/nagoya/feature/result/domain/ModelVoiceRepository.kt
+++
@@ -1,8 +0,0 @@
-package kuu.nagoya.feature.result.domain
-
-import android.content.res.AssetFileDescriptor
-import kuu.nagoya.data.tmprecorddata.entity.TmpRecordDataEntity
-
-interface ModelVoiceRepository {
-    fun loadModelVoice(tmpRecordDataEntity: TmpRecordDataEntity): AssetFileDescriptor
-}
